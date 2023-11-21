00c0cce8d483b892a2d858cb979d3b65dce5e670
787a68 <36852208+787a68@users.noreply.github.com>
2023-11-21 09:46:29 +0800

    up

--- Dtw.js
+++ Dtw.js
@@ -1,59 +1,67 @@
-var storedData = $persistentStore.read("dtwid");
-
-if (storedData) {
-  var data = JSON.parse(storedData);
-  var userId = data.userId.toString();
-  var openId = data.openId.toString();
-  var level = parseInt(data.level) + 1;
-
-  var response = $response.body;
-  var responseData = JSON.parse(response);
-  var questions = responseData.data.questions;
-
-  var answerRequests = questions.map(function (question, index) {
-    var answer = question.answer;
-    var questionId = question.id;
-    var num = index + 1;
-
-    var useTime = Math.ceil(Math.random() * 10); // 生成 1 到 10 之间的随机整数
-
-    return {
-      method: 'POST',
-      url: 'https://snans.huiyipro.com/api/answer/question',
-      headers: $request.headers, // 使用原有请求的头部信息
-      body: JSON.stringify({
-        wxUserId: userId,
-        openId: openId,
-        questionId: questionId,
-        answer: answer,
-        level: level,
-        num: num,
-        useTime: useTime,
-        theNth: level
-      })
-    };
-  });
-
-  // 批量发送答案请求
-  answerRequests.forEach(function (request, index) {
-    setTimeout(function () {
-      $httpClient.post(request, function (error, response, body) {
-        var questionId = questions[index].id;
-        if (!error && response.statusCode == 200) {
-          var answerData = JSON.parse(body);
-          if (answerData.success) {
-            console.log("答案提交成功，题目ID：" + questionId);
-          } else {
-            console.log("答案提交失败，题目ID：" + questionId);
-          }
-        } else {
-          console.log("答案提交失败，题目ID：" + questionId + "，错误信息：" + (error || "请求错误"));
-        }
-      });
-    }, index * 500); // 每次请求间隔0.5秒
-  });
-} else {
-  console.log("未找到存储的数据");
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-$done({});
+async function answerQuestion(userId, openId, level, question) {
+  const { id: questionId, answer, num } = question;
+  const useTime = Math.ceil(Math.random() * 10);
+
+  return new Promise(resolve => {
+    $httpClient.post(
+      {
+        url: 'https://snans.huiyipro.com/api/answer/question',
+        headers: {
+          ...$request.headers,
+          'Accept-Encoding': 'gzip,compress,br,deflate',
+        },
+        body: JSON.stringify({
+          wxUserId: userId,
+          openId,
+          questionId,
+          answer,
+          level,
+          num,
+          useTime,
+          theNth: level,
+        }),
+      },
+      (error, response, body) => {
+        const answerData = JSON.parse(body || '{}');
+        if (error || answerData.success === false) {
+          console.log(`答案提交失败，题目ID：${questionId}，错误信息：${error || '请求错误'}，${answerData.message || ''}`);
+        }
+        resolve(); // 标记当前请求已完成
+      }
+    );
+  });
+}
+
+async function main() {
+  const storedData = $persistentStore.read('dtwid');
+
+  if (storedData) {
+    const { userId, openId, level: storedLevel } = JSON.parse(storedData);
+    const level = parseInt(storedLevel) + 1;
+
+    const response = $response.body;
+    const { data: { questions } } = JSON.parse(response);
+
+    try {
+      for (const [index, question] of questions.entries()) {
+        if (index !== 0) {
+          await delay(1000); // 除第一个请求外，每次请求间隔1秒
+        }
+        await answerQuestion(userId, openId, level, question);
+      }
+    } catch (error) {
+      console.error('发生错误：', error);
+    } finally {
+      $done({});
+    }
+  } else {
+    console.log('未找到存储的数据');
+    $done({});
+  }
+}
+
+main();
